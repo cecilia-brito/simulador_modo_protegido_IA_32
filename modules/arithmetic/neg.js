99ae312b7e0d150c2749d4f13c14ccea55b9114a
caio <caioserenoos1@gmail.com>
2025-01-22 14:33:39 -0300

    neg inicio

--- modules/arithmetic/neg.js
+++ modules/arithmetic/neg.js
@@ -1,1 +1,15 @@
 //NEG DST         Negate DST (subtract it from 0)
+const neg = [
+    line=>{
+        return (line[0]&&line[1]&&line[2])&&[line[0], line[1], line[2]];
+    },
+    //step 1
+    (setVisual, cpuXram, getLinearAddress, cpu)=>{
+        const cs = cpu.segmentRegister.cs;
+        const codeSegment = cpu.segmentTable[cs];
+
+        const linearAddress = getLinearAddress("ip");
+        
+    }
+];
+export default neg;
