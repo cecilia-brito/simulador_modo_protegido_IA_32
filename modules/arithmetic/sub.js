fc263d7f30978c660da68546e943c00b09064808
caio <caioserenoos1@gmail.com>
2025-01-22 14:45:36 -0300

    inicio sub

--- modules/arithmetic/sub.js
+++ modules/arithmetic/sub.js
@@ -1,3 +1,14 @@
 //SUB DST,SRC      Subtract DST from SRC
-const sub = [];
+const sub = [
+    line => {
+        return(line[0]&&line[1]&&line[2]&&[line[0], line[1], line[2]]);
+    },
+    (setVisual, cpuXram, getLinearAddress, cpu) => {
+        const cs = cpu.segmentRegister.cs
+        const codeSegment = cpu.segmentTable[cs]
+
+        const linearAddress = getLinearAddress("ip")
+        
+    }
+];
 export default sub;
