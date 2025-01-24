05a9537918a2502d837904ac2dddb1541e947b56
canoeiro <caioserenoos1@gmail.com>
2025-01-24 09:49:21 -0300

    edição sub

--- modules/arithmetic/sub.js
+++ modules/arithmetic/sub.js
@@ -7,10 +7,32 @@ const sub = [
         // Passo 1
         const cs = cpu.segmentRegister.cs
         const codeSegment = cpu.segmentTable[cs]
-
+        // Mensagem barramento
+        cpuXram(
+            `bus endereço\n`
+            + `endereço linear = ${codeSegment.base} + ${cpu.offsetRegister.ip}\n`
+            + `endereço linear = ${codeSegment.base + cpu.offsetregister.ip}`,
+            "request",
+            codeSegment.base+cpu.offsetRegister.ip
+        )
         const linearAddress = getLinearAddress("ip")
-        
-    }
+        return false //porque?
+    },
+    (setVisual, cpuXram, getLinearAddress, cpu) => {
+        // Passo 2
+        const cs = cpuRegister.cs
+        const codeSegment = cpu.segmentTable[cs];
+        const linearAddress = getLinearAddress('ip')
+        cpuXram(
+            `bus dados\n`
+            + `Informações do endereço = MOV (SRC)`,
+            'get',
+            codeSegment.base+cpu.offsetRegister.ip
+        )
+        setVisual('offset', 'ip', cpu.offsetRegister.ip + 8)
+        return false
+    } 
+    
 ];
 
 /* 
