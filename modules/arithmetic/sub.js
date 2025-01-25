5316dd8a18bc145f2f525ab4d9d325b822c1e1bf
canoeiros <113272028+canoeiros@users.noreply.github.com>
2025-01-25 16:25:20 -0300

    aaaaaa

--- modules/arithmetic/sub.js
+++ modules/arithmetic/sub.js
@@ -15,6 +15,7 @@ const sub = [
             codeSegment.base + cpu.offsetRegister.ip
         )
         getLinearAddress('ip');
+        console.log('step 1');
     },
     // step 2 (Reconhecimento: SUB)
     (setVisual, cpuXram, getLinearAddress, cpu) => {
@@ -44,6 +45,7 @@ const sub = [
         setVisual('offset', 'di', parseInt(control.line[1], 16));
         setVisual('offset', 'si', parseInt(control.line[1], 16));
         setVisual('offset', 'ip', cpu.offsetRegister.ip + 4);
+        console.log('step 4');
     },
     // step 5
     (setVisual, cpuXram, getLinearAddress, cpu) => {
@@ -54,15 +56,60 @@ const sub = [
             endereço linear = ${dataSegment.base.toString(16)} + ${cpu.offsetRegister.di.toString(16)}<br/>
             endereço linear = ${(dataSegment.base + cpu.offsetRegister.di).toString(16)}`,
             'request',
-            dataSegment.base + cpu.offsetRegister.si
+            dataSegment.base + cpu.offsetRegister.di
         );
         getLinearAddress('si');
     },
     // step 6 (Escrever dados em EAX)
     (setVisual, cpuXram, getLinearAddress, cpu) => {
         const ram = cpu.ram;
+        const linearAddress = getLinearAddress('si');
+        const data =
+            ram[linearAddress + 3] * 0x1000000 +
+            ram[linearAddress + 2] * 0x10000 +
+            ram[linearAddress + 1] * 0x100 +
+            ram[linearAddress];
+            cpuXram(
+          `bus dados<br/>
+          dados: ${data}`,
+          'get',
+          linearAddress
+        );
+        setVisual("geral", "eax", data);
+      },
+    // step 7 (Reconhecimento Origem)
+    (setVisual, cpuXram, getLinearAddress, cpu) => {
+        sub[1](setVisual, cpuXram, getLinearAddress, cpu);
+    },
+    // step 8
+    (setVisual, cpuXram, getLinearAddress, cpu) => {
+        const control = cpu.controlUnity;
+        const linearAddress = getLinearAddress('ip');
+        cpuXram(
+            `bus dados<br/>
+            dados: ${control.line[2]}`,
+            'get', 
+            linearAddress
+        );
+        setVisual('offset', 'si', parseInt(control.line[2], 16));
+        setVisual('offset', 'si', parseInt(cpu.offsetRegister + 4));
+    },
+    // step 9
+    (setVisual, cpuXram, getLinearAddress, cpu) => {
         const ds = cpu.segmentRegister.ds;
-        const codeSegment = cpu.segmentTable[ds];
+        const dataSegment = cpu.segmentTable[ds];
+        cpuXram(
+            `bus endereço<br/>
+            endereço linear = ${dataSegment.base.toString(16)} + ${cpu.offsetRegister.di.toString(16)}<br/>
+            endereço linear = ${(dataSegment.base + cpu.offsetRegister.di).toString(16)}`,
+            'request',
+            dataSegment.base + cpu.offsetRegister.di
+        )
+        getLinearAddress('si');
+    },
+    // step 10
+    (setVisual, cpuXram, getLinearAddress, cpu) => {
+        const ram = cpu.ram;
         const linearAddress = getLinearAddress('si');
         const data =
             ram[linearAddress + 3] * 0x1000000 +
@@ -73,51 +120,22 @@ const sub = [
             `bus dados<br/>
             dados: ${data}`,
             'get',
-            codeSegment.base + cpu.offsetRegister.si
-        );
-        setVisual("geral", "eax", data);
-      },
-    // step 7 (Reconhecimento Origem)
-    (setVisual, cpuXram, getLinearAddress, cpu) => {
-        sub[3](setVisual, cpuXram, getLinearAddress, cpu);
-        console.log('step 7');
-    },
-    // step 8
-    (setVisual, cpuXram, getLinearAddress, cpu) => {
-        const control = cpu.controlUnity;
-        const linearAddress = getLinearAddress('ip');
-        const data = control.line[2];
-        cpuXram(
-            `bus dados<br/>
-            dados: ${data.toString(16)}`,
-            'get', 
             linearAddress
-        );
-        setVisual('offset', 'si', parseInt(control.line[2], 16));
-        setVisual('offset', 'ip', parseInt(cpu.offsetRegister.ip + 4));
-    },
-    // step 9
-    (setVisual, cpuXram, getLinearAddress, cpu) => {
-        sub[5](setVisual, cpuXram, getLinearAddress, cpu);
-    },
-    // step 10
-    (setVisual, cpuXram, getLinearAddress, cpu) => {
-        const ram = cpu.ram;
-        const ds = cpu.segmentRegister.ds;
-        const codeSegment = cpu.segmentTable[ds];
-        const linearAddress = getLinearAddress('si');
-        const data =
-            ram[linearAddress + 3] * 0x1000000 +
-            ram[linearAddress + 2] * 0x10000 +
-            ram[linearAddress + 1] * 0x100 +
-            ram[linearAddress];
-        cpuXram(
-            `bus dados<br/>
-            dados: ${data}`,
-            'request',
-            codeSegment.base + cpu.offsetRegister.si
         )
         setVisual('geral', 'ebx', data);
+    },
+    // step 11
+    (setVisual, cpuXram, getLinearAddress, cpu) => {
+        const ds = cpu.segmentRegister.ds;
+        const dataSegment = cpu.segmentTable[ds];
+        cpuXram(
+            `bus endereço<br/>
+            endereço linear = ${dataSegment.base.toString(16)} + ${cpu.offsetRegister.di.toString(16)}<br/>
+            endereço linear = ${(dataSegment.base + cpu.offsetRegister.di).toString(16)}`,
+            'request',
+            dataSegment.base + cpu.offsetRegister.di
+        );
+        getLinearAddress("di");
     },
     // step 12
     (setVisual, cpuXram, getLinearAddress, cpu) => {
@@ -125,11 +143,11 @@ const sub = [
         const ebx = cpu.geralRegister.ebx;
         let subtraction = eax - ebx;
         setVisual('geral', 'eax', subtraction);
-        cpu.geralRegister.eax -= cpu.geralRegister.ebx; 
         const linearAddress = getLinearAddress('di');
         cpuXram(
-            `bus dados<br/>`,
-            'request',
+            `bus dados<br/>
+            dados: ${cpu.geralRegister.eax.toString(16)}`,
+            'request', 
             linearAddress
         );
         setVisual('ram', linearAddress, cpu.geralRegister.eax);
@@ -137,14 +155,3 @@ const sub = [
     }
 ];
 export default sub;
-
-/* 
-Passos:
-1. Request p/ RAM da destino
-2. Informações da RAM destino (Edita EAX)
-3. Request p/ RAM destino 
-4. Informações da Ram Destino (Edita EAX)
-5. Operação EAX -> EAX - EBX (Edita EAX)
-6. Atualiza Flags
-
-*/
