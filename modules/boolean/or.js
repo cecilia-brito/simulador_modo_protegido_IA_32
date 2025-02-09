e5266ce0d456204a31415f6e18e40a608ea05950
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2025-02-09 18:31:52 -0300

    instrução jxx feita

--- modules/boolean/or.js
+++ modules/boolean/or.js
@@ -12,8 +12,8 @@ const or  = [
         cpuXram(
             //desc
             `bus endereço<br/>
-            endereço linear = ${codeSegment.base.toString(16)} + ${cpu.offsetRegister.ip.toString(16)}<br/>
-            endereço linear = ${(codeSegment.base + cpu.offsetRegister.ip).toString(16)}`,
+            endereço linear = ${showHexa(codeSegment.base)} + ${showHexa(cpu.offsetRegister.ip)}<br/>
+            endereço linear = ${showHexa(codeSegment.base + cpu.offsetRegister.ip)}`,
             //request = "->"
             //get = "<-"
             //"" = [] (quadrado)
@@ -61,7 +61,7 @@ const or  = [
         cpuXram(
             //desc
             `Bus Dados<br>`
-           +`${data.toString(16)}`,
+           +`${showHexa(data)}`,
             //request = "->"
             //get = "<-"
             //"" = [] (quadrado)
@@ -79,8 +79,8 @@ const or  = [
         const dataSegment = cpu.segmentTable[ds];
         cpuXram(
             `bus endereço<br/>
-            endereço linear = ${dataSegment.base.toString(16)} + ${cpu.offsetRegister.si.toString(16)}<br/>
-            endereço linear = ${(dataSegment.base + cpu.offsetRegister.si).toString(16)}`,
+            endereço linear = ${showHexa(dataSegment.base)} + ${showHexa(cpu.offsetRegister.si)}<br/>
+            endereço linear = ${showHexa(dataSegment.base + cpu.offsetRegister.si)}`,
             "request",
             dataSegment.base+cpu.offsetRegister.si
         );
@@ -97,7 +97,7 @@ const or  = [
         console.log(linearAddress)
         cpuXram(
             `bus dados<br/>
-            dados: ${data.toString(16)}`,
+            dados: ${showHexa(data)}`,
             "get",
             codeSegment.base+cpu.offsetRegister.si
         );
@@ -115,7 +115,7 @@ const or  = [
         cpuXram(
             //desc
             `Bus Dados<br>`
-           +`${data.toString(16)}`,
+           +`${showHexa(data)}`,
             //request = "->"
             //get = "<-"
             //"" = [] (quadrado)
@@ -144,7 +144,7 @@ const or  = [
         console.log(linearAddress)
         cpuXram(
             `bus dados<br/>
-            dados: ${data.toString(16)}`,
+            dados: ${showHexa(data)}`,
             "get",
             codeSegment.base+cpu.offsetRegister.si
         );
@@ -159,15 +159,31 @@ const or  = [
         const linearAddress = getLinearAddress("di");
         cpuXram(
             `bus dados<br/>
-            endereço linear = ${dataSegment.base.toString(16)} + ${cpu.offsetRegister.di.toString(16)}<br/>
-            endereço linear = ${linearAddress.toString(16)}<br/>
-            dados: ${cpu.geralRegister.eax.toString(16)}`,
+            endereço linear = ${showHexa(dataSegment.base)} + ${showHexa(cpu.offsetRegister.di)}<br/>
+            endereço linear = ${showHexa(linearAddress)}<br/>
+            dados: ${showHexa(cpu.geralRegister.eax)}`,
             "request",
             linearAddress
         )
+//         Flags Affected
+// The OF and CF flags are cleared; the SF, ZF, and PF flags are set according to the result. The state of the AF flag is
+// undefined
+        cpu.flag.overflow =  false;
+        cpu.flag.carry = false;
+        cpu.flag.zero = cpu.geralRegister == 0;
+        let twoComp = (cpu.geralRegister.eax>>>0).toString(2);
+        cpu.flag.zero = twoComp == 0;
+        let test_parity = twoComp;
+        test_parity = test_parity.slice(test_parity.length - 8, test_parity.length - 1);
+        cpu.flag.parity = test_parity == "11111111";        
+        cpu.flag.sign = twoComp[0]==="1"&&!twoComp.split("").every(a=>a==="1")
         console.log("step 10")
         setVisual("ram", linearAddress, cpu.geralRegister.eax)
         return true;
     }
 ];
 export default or;
+
+function showHexa(value, pad = 8){
+    return value.toString(16).padStart(pad, "0");
+}
--- modules/compare_test/jxx.js
+++ modules/compare_test/jxx.js
@@ -1,46 +1,192 @@
 //Jxx ADDR        Conditional jumps based on flag
-
 // temos que XX representa:
-
 // G        >
-
 // GE       >=
-
 // E          =
-
 // NE       !=
-
 // L         <
-
 // LE       <=
 
 const jxx = [
     (array) => {
         const regex = /(j{1})([a-z]{1,2})/gi;
-        if(array[0] != undefined){
+        console.log(array)
+        if(array[0] != undefined && array[1] != undefined){
             let matches = array[0].matchAll(regex);
-            let condition = matches[2];
+            let condition = [...matches]
+            condition = condition[0][2].toUpperCase()
             switch (condition) {
                 case "G":
-                    return array;
+                    return ["jxx", array[1], "G"];
                 case "GE":
-                    return array;
+                    return ["jxx", array[1], "GE"];
                 case "E":
-                    return array;
+                    return ["jxx", array[1], "E"];
                 case "NE":
-                    return array;
+                    return ["jxx", array[1], "NE"];
                 case "LE":
-                    return array;
+                    return ["jxx", array[1], "LE"];
                 case "L":
-                    return array;
+                    return ["jxx", array[1], "L"];
                 default:
                     break;
             }
+        
         }
     },
-    (setVisual, cpuXRam,getLinearAddress, cpu)=>{
+    (setVisual, cpuXram, getLinearAddress, cpu)=>{
+        const cs = cpu.segmentRegister.cs;
+        const codeSegment = cpu.segmentTable[cs];
+		getLinearAddress("ip");
+        cpuXram(
+            //desc
+            `bus endereço<br/>
+            endereço linear = ${showHexa(codeSegment.base)} + ${showHexa(cpu.offsetRegister.ip)}<br/>
+            endereço linear = ${showHexa(codeSegment.base + cpu.offsetRegister.ip)}`,
+            //request = "->"
+            //get = "<-"
+            //"" = [] (quadrado)
+            "request",
+            //posição na ram
+            codeSegment.base+cpu.offsetRegister.ip
+        );
+        console.log("step 1")
+        return false;
+    },  (setVisual, cpuXram, getLinearAddress, cpu)=>{
+        const linearAddress = getLinearAddress('ip');
+       
+        cpuXram(
+            //desc
+            `Bus Dados<br>`
+           +`${'J'+cpu.controlUnity.line[2]}`,
+            //request = "->"
+            //get = "<-"
+            //"" = [] (quadrado)
+            "get",
+            //posição na ram
+            linearAddress
+        );
+        setVisual("offset", "ip", cpu.offsetRegister.ip + 4);
+        console.log("step 2")
+        return false;
+    },(setVisual, cpuXram, getLinearAddress, cpu) =>{
+        jxx[1](setVisual, cpuXram, getLinearAddress, cpu);
+    }, (setVisual, cpuXram, getLinearAddress, cpu)=>{
+        let condition = cpu.controlUnity[2]
+        switch (condition) {
+            case "G":
+                if(cpu.flag.zero == false && cpu.flag.sign == false){
+                    const cs = cpu.segmentRegister.cs;
+                    const codeSegment = cpu.segmentTable[cs];
+                    const control = cpu.controlUnity
+                    const dataAdress = (parseInt(control.line[1], 16));
+
+                    setVisual("offset", "ip", dataAdress)
+                    console.log(cpu)
+
+                    cpuXram(
+                        
+                        `Pulando para o endereço: ${showHexa(control.line[1])}`,
+
+                        codeSegment.base+cpu.offsetRegister.ip
+                    );
+                }
+            case "GE":
+                if(cpu.flag.sign == cpu.flag.overflow){
+                    const cs = cpu.segmentRegister.cs;
+                    const codeSegment = cpu.segmentTable[cs];
+                    const control = cpu.controlUnity
+                    const dataAdress = (parseInt(control.line[1], 16));
+
+                    setVisual("offset", "ip", dataAdress)
+                    console.log(cpu)
+
+                    cpuXram(
+                        
+                        `Pulando para o endereço: ${showHexa(control.line[1])}`,
+
+                        codeSegment.base+cpu.offsetRegister.ip
+                    );
+                }
+            case "E":
+                if(cpu.flag.zero == true){
+                    const cs = cpu.segmentRegister.cs;
+                    const codeSegment = cpu.segmentTable[cs];
+                    const control = cpu.controlUnity
+                    const dataAdress = (parseInt(control.line[1], 16));
+
+                    setVisual("offset", "ip", dataAdress)
+                    console.log(cpu)
+
+                    cpuXram(
+                        
+                        `Pulando para o endereço: ${showHexa(control.line[1])}`,
+
+                        codeSegment.base+cpu.offsetRegister.ip
+                    );
+                }
+            case "NE":
+                if(cpu.flag.zero == 0){
+                    const cs = cpu.segmentRegister.cs;
+                    const codeSegment = cpu.segmentTable[cs];
+                    const control = cpu.controlUnity
+                    const dataAdress = (parseInt(control.line[1], 16));
+            
+                    setVisual("offset", "ip", dataAdress)
+                    console.log(cpu)
+            
+                    cpuXram(
+                        
+                        `Pulando para o endereço: ${showHexa(control.line[1])}`,
+            
+                        codeSegment.base+cpu.offsetRegister.ip
+                    ); 
+                }
+            case "LE":
+                if(cpu.flag.zero == true || cpu.flag.sign != cpu.flag.overflow){
+                    const cs = cpu.segmentRegister.cs;
+                    const codeSegment = cpu.segmentTable[cs];
+                    const control = cpu.controlUnity
+                    const dataAdress = (parseInt(control.line[1], 16));
+
+                    setVisual("offset", "ip", dataAdress)
+                    console.log(cpu)
+
+                    cpuXram(
+                        
+                        `Pulando para o endereço: ${showHexa(control.line[1])}`,
+
+                        codeSegment.base+cpu.offsetRegister.ip
+                    );
+                }
+
+            case "L":
+                if(cpu.flag.overflow != cpu.flag.sign){
+					const cs = cpu.segmentRegister.cs;
+					const codeSegment = cpu.segmentTable[cs];
+					const control = cpu.controlUnity
+					const dataAdress = (parseInt(control.line[1], 16));
+
+					setVisual("offset", "ip", dataAdress)
+					console.log(cpu)
+
+					cpuXram(
+						
+						`Pulando para o endereço: ${showHexa(control.line[1])}`,
+
+						codeSegment.base+cpu.offsetRegister.ip
+					);
+				}
+            default:
+                break;
+        
+        }
         return true;
     }
 
 ];
 export default jxx;
+
+function showHexa(value, pad = 8){
+    return value.toString(16).padStart(pad, "0");
+}
