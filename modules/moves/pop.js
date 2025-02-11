d22c65b326a22d521f6f0f7ad59906eec026a66f
canoeiros <113272028+canoeiros@users.noreply.github.com>
2025-02-10 23:28:05 -0300

    ssasjsj

--- modules/moves/pop.js
+++ modules/moves/pop.js
@@ -3,7 +3,7 @@ const pop = [
     line => {
         return(line[0]&&line[1]&&!line[2]&&[line[0], line[1]]);
     },
-
+    // Pegando Endereço pop
     (setVisual, cpuXram, getLinearAddress, cpu) => {
         const cs = cpu.segmentRegister.cs
         const codeSegment = cpu.segmentTable[cs]
@@ -17,9 +17,7 @@ const pop = [
         getLinearAddress('ip')
         console.log('p1')
     },
-
-
-    // Passo 2 
+    // Passo 2 Lendo Pop
     (setVisual, cpuXram, getLinearAddress, cpu, data = 'POP') => {
         const linearAddress = getLinearAddress('ip')
         cpuXram(
@@ -30,25 +28,7 @@ const pop = [
         )   
         setVisual('offset','ip', cpu.offsetRegister.ip + 4)
     },
-
-
-    // Passo 3 (Passo 1 - Reconhecimento DST)
-    (setVisual, cpuXram, getLinearAddress, cpu) => {
-        pop[1](setVisual, cpuXram, getLinearAddress, cpu)
-        console.log('p3')   
-    },
-
-
-    //Passo 4 (Controle dos Ponteiros de SI e DI)
-    (setVisual, cpuXram, getLinearAddress, cpu) => {
-        const control = cpu.controlUnity
-        const linearAddress = getLinearAddress('ip')
-        pop[2](setVisual, cpuXram, getLinearAddress, cpu, control.line[1])
-        setVisual('offset', 'di', parseInt(control.line[1], 16))
-    },
-
-
-    // Passo 5 (Request de Dados da Pilha)
+    // Passo 3 Retirar dados da Pilha
     (setVisual, cpuXram, getLinearAddress, cpu) => {
         const ss = cpu.segmentRegister.ss
         const stackSegment = cpu.segmentTable[ss]
@@ -61,25 +41,38 @@ const pop = [
         )
         getLinearAddress('sp')
     },
-
-
-    // Passo 6 (Pegando dados da Pilha)
+    // Escrever em EAX os dados da pilha
     (setVisual, cpuXram, getLinearAddress, cpu) => {
         const linearAddress = getLinearAddress('sp')
+        setVisual('offset', 'sp', cpu.offsetRegister.sp+4);
+        getLinearAddress('sp')
+        const data = 
+            ram[linearAddress + 4] * 0x1000000 +
+            ram[linearAddress + 3] * 0x10000 +
+            ram[linearAddress + 2] * 0x100 +
+            ram[linearAddress + 1]
         cpuXram(
             `bus dados<br/>
             dados `,
             'get',
             linearAddress
         )
-        const data = 
-            ram[linearAddress - 3] *  +
-            ram[linearAddress - 2] *  0x100 +
-            ram[linearAddress - 1] * 0x10000 +
-            ram[linearAddress] * 0x1000000
-        setVisual('ram', linearAddress-3, cpu.offsetRegister.ip); //?
-        setVisual('offset', 'sp', cpu.offsetRegister.sp-4);
         setVisual('geral','eax', data)
+    },
+
+    // Passo 3 (Passo 1 - Reconhecimento DST)
+    (setVisual, cpuXram, getLinearAddress, cpu) => {
+        pop[1](setVisual, cpuXram, getLinearAddress, cpu)
+        console.log('p3')   
+    },
+
+
+    //Passo 4 (Controle dos Ponteiros de DI)
+    (setVisual, cpuXram, getLinearAddress, cpu) => {
+        const control = cpu.controlUnity
+        const linearAddress = getLinearAddress('ip')
+        pop[2](setVisual, cpuXram, getLinearAddress, cpu, control.line[1])
+        setVisual('offset', 'di', parseInt(control.line[1], 16))
     },
 
 
